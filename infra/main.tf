215593cd137831d1b4f9826f364be744df22d071
franco97sassi <franco.sassi97@gmail.com>
2026-06-14 17:38:30 -0300

    Configure Terraform remote backend

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "clientes-aws-terraform-state-851725347003"
+    key    = "clientes-aws/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
